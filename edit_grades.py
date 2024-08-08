c70209781339130736788519525100e6178bd39b
Kai <aeth.kr@gmail.com>
2024-08-07 22:32:55 -0500

    Test

--- edit_grades.py
+++ edit_grades.py
@@ -1,5 +1,5 @@
 import gpa_calc
-
+a
 # TODO
 # user should be able to edit course details (maybe through a menu)
 # if the user decides to change course details such as credit hours or grade, recalculate these to reflect the changes
